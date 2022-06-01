93c659499ab86fa9b87bba5e36a2a33b0dfadc31
Patrick Ingraham <pingraham@lsst.org>
2022-06-01 15:05:14 -0700

    Initial commit for 2022-06A

--- bash/lsst/sitcom/package_branch_creation.sh
+++ bash/lsst/sitcom/package_branch_creation.sh
@@ -5,9 +5,9 @@
 # This script is not ready to be run yet
 
 # Ticket associated with this run
-TICKET='DM-34844'
+TICKET='DM-35030'
 # Commit message to open the branch
-MESSAGE="Initial Branch Creation for 2022-05B AuxTel Support. Empty commit with no content changed"
+MESSAGE="Initial Branch Creation for 2022-06A AuxTel Support. Empty commit with no content changed"
 
 
 PACKAGES=('lsst-ts/ts_observatory_control' 'lsst-ts/ts_observing_utilities' 'lsst-ts/ts_standardscripts' 'lsst-ts/ts_externalscripts' 'lsst-ts/cwfs' 'lsst/atmospec')
@@ -43,28 +43,10 @@ do
   echo "$pkg branch preparation completed \n"
 done
 
-
-
-
-
-
+### Just keep this for safe keeping as it is what the above was meant to replicate
 # cd ~/develop
 # git clone git@github.com:lsst-ts/ts_observing_utilities.git
 # cd ts_observing_utilities
-# git checkout -b tickets/DM-34581
-# git commit -m "Initial Branch Creation for 2022-05 AuxTel Support"
-# git push --set-upstream origin tickets/DM-34581
-
-# cd ~/develop
-# git clone git@github.com:lsst-ts/ts_standardscripts.git
-# cd ts_standardscripts
-# git checkout -b tickets/DM-34581
-# git commit -m "Initial Branch Creation for 2022-05 AuxTel Support"
-# git push --set-upstream origin tickets/DM-34581
-
-# cd ~/develop
-# git clone git@github.com:lsst-ts/ts_externalscripts.git
-# cd ts_externalscripts
 # git checkout -b tickets/DM-34581
 # git commit -m "Initial Branch Creation for 2022-05 AuxTel Support"
 # git push --set-upstream origin tickets/DM-34581
